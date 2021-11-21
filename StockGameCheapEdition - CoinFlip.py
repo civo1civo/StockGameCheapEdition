7e002543b1f2b0b705eebe356c0771c93dece370
civo1civo <83471475+civo1civo@users.noreply.github.com>
2021-11-21 16:02:17 +0100

    Update StockGameCheapEdition - CoinFlip.py
    This is the best version of all the stockgamecheapedition games.
    Please use this one.

--- StockGameCheapEdition - CoinFlip.py
+++ StockGameCheapEdition - CoinFlip.py
@@ -23,7 +23,7 @@ while game == True:
               str(maximumStocks) + ' /// Price Stocks: €' + str(priceStocks) + ' /// Amount Stocks:' +
               str(amountStocks) +'\n')
         while info == 'gone':
-            print('Commands: buyStocks, sellStocks, buyFirewall, coinflip, showInfo')
+            print('Commands: buyStocks, sellStocks, buyFirewall, coinFlip, showInfo')
             x = input()
             if x == str('sellStocks'):
                 sellPrice = random.randint(1, 8)
@@ -34,20 +34,20 @@ while game == True:
                     money = int(money) / 2
                     print('You have been hacked, half of your money is gone.')
                 if int(theft) == 2:
-                    print('The stocks plummeted, you lost all the money.')
+                    print('\nThe stocks plummeted, you lost all the money.')
                     sellPrice = 0
                 profit = int(sellPrice) * int(amountStocks)
                 if year < 2031 :
-                    print('\nThe sell price this year is €' + str(sellPrice) + '. You had ' + str(amountStocks) +
+                    print('\n'+'\n'+'The sell price this year is €' + str(sellPrice) + '. You had ' + str(amountStocks) +
                       ' stocks, so you get €' + str(profit) + '. Your yearly tax is €5.')
                     money = int(money) + int(profit) - 5
                 if year >= 2031:
                     print('\nThe sell price this year is €' + str(sellPrice) + '. You had ' + str(amountStocks) +
                       ' stocks, so you get €' + str(profit) + '. Your yearly tax is €10.')
                     money = int(money) + int(profit) - 10
-                print('You now have €' + str(money) + '.')
+                print('You now have €' + str(money) + '.' + '\n')
                 if int(money) < 0:
-                    print('You lost all your money.')
+                    print('You lost all your money.' + '\n Thank you for playing!')
                     info = 0
                     game = False
                 else:
@@ -59,18 +59,18 @@ while game == True:
                 print('How many stocks do you want to buy? (number)')
                 x = input()
                 if int(x) < 0:
-                    print('The minimum amount is 0.')
+                    print('The minimum amount is 0.'+'\n')
                 elif int(x) + int(ownedStocks) > int(maximumStocks):
                     print('The maximum amount of stocks you can own is 50. ' + 'You own ' + str(ownedStocks) + ' stocks.')
                 elif int(x) * int(priceStocks) <= int(money):
-                    print('You bought ' + str(x) + ' stocks.')
+                    print('You bought ' + str(x) + ' stocks.'+'\n')
                     money = int(money) - int(priceStocks) * int(x)
                     amountStocks = int(amountStocks) + int(x)
                     info = 'show'
                 else:
                     print('You do not have enough money to buy ' + str(x) +' stocks.')
             elif x == str('buyFirewall'):
-                print('Do you want to buy a firewall for €150? Type yes or no')
+                print('\nDo you want to buy a firewall for €150? Type yes or no.')
                 x = input()
                 if x == 'yes':
                     if 150 <= int(money):
@@ -86,37 +86,40 @@ while game == True:
             elif x == str('showInfo'):
                 print('\nInfo:'+'\nbuyStocks = By typing this command you can buy stocks.Fill in a number, if you fill in somthing that is not a number, the game will crash.'+
       '\nsellStocks = By typing this command, you will go to a new year and sell your stocks at a random price.'
-      '\nbuyFirewall = By typing this command, you will have the option to buy a firewall that protects you from getting hacked.'+'\n')
-            elif x == str('coinflip'):
+      '\nbuyFirewall = By typing this command, you will have the option to buy a firewall that protects you from getting hacked.'+
+      '\ncoinFlip = By typing this command, you have the chance to double or lose all your money. The stocks you bought will not be affected.')
+            elif x == str('coinFlip'):
                 print('Do you want to go all in and see if you can double your money? Type yes or no'+'\nNote: You can lose all of it.')
                 x = input()
-                if x == 'yes':
-                    print('Heads or Tails?')
+                if x == 'yes' or 'Yes':
+                    print('\nHeads or Tails?')
                     x = input()
                     if x == 'Heads':
                         Heads = random.randint(1 , 2)
-                        print('\n' + str(Heads) + '\n')
                         if Heads == 1:
+                            print('\n'+'Heads'+'\n')
                             money = int(money) * 2
+                            print(' You won, you now have €' + str(money)+'.' +'\n')
                             info = 'show'
                         elif Heads == 2:
                             money = 0
-                            print('You lost all your money.')
-                            if money == '0' and ownedStocks == '0' :
+                            print('You lost all your money.'+'\n')
+                            if amountStocks == 0 :
                                 info = 0
                                 game = False
                             else:
                                 info = 'show'
                     elif x == 'Tails':
                         Tails = random.randint(1 , 2)
-                        print('\n' + str(Tails) + '\n')
                         if Tails == 2:
+                            print('\n'+'Tails'+'\n')
                             money = int(money) * 2
+                            print('You won, you now have €' + str(money) + '.' + '\n')
                             info = 'show'
-                        elif Tails == 1:
+                        elif Tails == 1 :
                             money = 0
-                            print('You lost all your money.')
-                            if money == '0' and ownedStocks == '0' :
+                            print('You lost all your money.'+'\n')
+                            if amountStocks == 0 :
                                 info = 0
                                 game = False
                             else:
